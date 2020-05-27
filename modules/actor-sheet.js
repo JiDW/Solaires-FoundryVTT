32edf8b634795f96b6bb53108be259f9d75c713d
Mathias Latournerie <jidewe@gmail.com>
2020-05-27 03:06:47 +0200

    Debut de la fiche perso

---
+++ modules/actor-sheet.js
@@ -0,0 +1,62 @@
+/**
+ * Extend the basic ActorSheet with some very simple modifications
+ * @extends {ActorSheet}
+ */
+export class SolairesActorSheet extends ActorSheet {
+
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["solaires", "sheet", "actor"],
+      template: "systems/solaires/templates/actor-sheet.html",
+      width: 600,
+      height: 600,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
+      dragDrop: [{ dragSelector: ".item-list .item", dropSelector: null }]
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  getData() {
+    const data = super.getData();
+    data.dtypes = ["String", "Number", "Boolean"];
+    return data;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Update Inventory Item
+    html.find('.item-edit').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      const item = this.actor.getOwnedItem(li.data("itemId"));
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find('.item-delete').click(ev => {
+      const li = $(ev.currentTarget).parents(".item");
+      this.actor.deleteOwnedItem(li.data("itemId"));
+      li.slideUp(200, () => this.render(false));
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  setPosition(options = {}) {
+    const position = super.setPosition(options);
+    const sheetBody = this.element.find(".sheet-body");
+    const bodyHeight = position.height - 192;
+    sheetBody.css("height", bodyHeight);
+    return position;
+  }
+}
---
+++ modules/actor.js
@@ -0,0 +1,30 @@
+/**
+ * Extend the base Actor entity by defining a custom roll data structure
+ * @extends {Actor}
+ */
+export class SolairesActor extends Actor {
+
+    /** @override */
+    getRollData() {
+      const data = super.getRollData();
+      const shorthand = game.settings.get("worldbuilding", "macroShorthand");
+  
+      // Re-map all attributes onto the base roll data
+      if ( !!shorthand ) {
+        for ( let [k, v] of Object.entries(data.attributes) ) {
+          if ( !(k in data) ) data[k] = v.value;
+        }
+        delete data.attributes;
+      }
+  
+      // Map all items data using their slugified names
+      data.items = this.data.items.reduce((obj, i) => {
+        let key = i.name.slugify({strict: true});
+        let itemData = duplicate(i.data);
+        obj[key] = itemData;
+        return obj;
+      }, {});
+      return data;
+    }
+  }
+  
